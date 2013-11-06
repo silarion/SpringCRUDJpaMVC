ecd0d3b9d779caedbf4c11a2952ad8f329b8aada
silarion <arnaud.gillet@gmail.com>
2013-11-06 23:00:18 +0100

    Utilisation de BeanDefinitionBuilder

--- SpringCRUDJpaMVC/src/main/java/com/intech/test/ControllersDefinitions.java
+++ SpringCRUDJpaMVC/src/main/java/com/intech/test/ControllersDefinitions.java
@@ -7,13 +7,14 @@ import javax.persistence.metamodel.EntityType;
 import javax.persistence.metamodel.Metamodel;
 
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.ConstructorArgumentValues;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.GenericTypeResolver;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
 
 public class ControllersDefinitions implements ImportBeanDefinitionRegistrar {
 
@@ -27,18 +28,44 @@ public class ControllersDefinitions implements ImportBeanDefinitionRegistrar {
 		EntityManagerFactory emf = registry.getParentBeanFactory().getBean(
 				EntityManagerFactory.class);
 
+		// entityManager
+		BeanDefinitionBuilder builder = BeanDefinitionBuilder
+				.rootBeanDefinition("org.springframework.orm.jpa.SharedEntityManagerCreator");
+		builder.setFactoryMethod("createSharedEntityManager");
+		builder.addConstructorArgReference("entityManagerFactory");
+		AbstractBeanDefinition emfDef = builder.getRawBeanDefinition();
+		// bean.setSource(source);
+
+		// repoFactory
+		builder = BeanDefinitionBuilder
+				.rootBeanDefinition(JpaRepositoryFactory.class);
+		builder.addConstructorArgValue(emfDef);
+		registry.registerBeanDefinition("repositoryFactory",
+				builder.getBeanDefinition());
+
 		// creation controller pour chaque Entity s'il n'existe pas
 		Metamodel model = emf.getMetamodel();
 		for (EntityType type : model.getEntities()) {
 			Class modelClass = type.getBindableJavaType();
 
 			if (!hasController(modelClass, registry)) {
+				// definition du repository
+				// GenericBeanDefinition repoDef = new GenericBeanDefinition();
+				// repoDef.setFactoryBeanName("repositoryFactory");
+				// repoDef.setFactoryMethodName("getRepository");
+				// ConstructorArgumentValues constructorArgumentValues = new
+				// ConstructorArgumentValues();
+				// constructorArgumentValues
+				// .addGenericArgumentValue(JpaRepository.class);
+				// repoDef.setConstructorArgumentValues(constructorArgumentValues);
+				// registry.registerBeanDefinition(modelClass.getSimpleName()
+				// .toLowerCase() + "Repository", repoDef);
+
 				// definition du Controller
-				GenericBeanDefinition newDef = new GenericBeanDefinition();
-				newDef.setBeanClass(CRUDController.class);
-				ConstructorArgumentValues constructorArgumentValues = new ConstructorArgumentValues();
-				constructorArgumentValues.addGenericArgumentValue(modelClass);
-				newDef.setConstructorArgumentValues(constructorArgumentValues);
+				builder = BeanDefinitionBuilder
+						.rootBeanDefinition(CRUDController.class);
+				builder.addConstructorArgValue(modelClass);
+				// builder.addPropertyValue("repository", value);
 
 				// ajout definition du Jparepository
 				// RepositoryBeanDefinitionBuilder rbdb = new
@@ -54,7 +81,8 @@ public class ControllersDefinitions implements ImportBeanDefinitionRegistrar {
 				// newDef.setPropertyValues(propertyValues);
 
 				registry.registerBeanDefinition(modelClass.getSimpleName()
-						.toLowerCase() + "Controller", newDef);
+						.toLowerCase() + "Controller",
+						builder.getBeanDefinition());
 				System.out.println("Creation Controller " + modelClass);
 			}
 
